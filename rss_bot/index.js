e08c307497bb0c0d5b5b8eb6db627405af5d98ea
Anthony Zegnal <anthonyzegnal05@gmail.com>
2023-07-15 17:46:48 +0200

    j'ai refait la meme structure que quiz_bot

--- rss_bot/index.js
+++ rss_bot/index.js
@@ -1,16 +1,78 @@
-const Parser = require('rss-parser');
-const parser = new Parser();
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, IntentsBitField } = require('discord.js');
+const { token } = require('./config.json');
 
-(async () => {
-  try {
-    const feed = await parser.parseURL('https://nyaa.si/?page=rss');
-    
-    console.log(`Titre du flux : ${feed.title}`);
-    
-    feed.items.forEach(item => {
-      console.log(`Titre de l'article : ${item.title}`);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-})();
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessages] });
+
+client.cooldowns = new Collection();
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.once(Events.ClientReady, () => {
+	console.log('Ready!');
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	const { cooldowns } = client;
+
+	if (!cooldowns.has(command.data.name)) {
+		cooldowns.set(command.data.name, new Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.data.name);
+	const defaultCooldownDuration = 3;
+	const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+	if (timestamps.has(interaction.user.id)) {
+		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const expiredTimestamp = Math.round(expirationTime / 1000);
+			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+		}
+	}
+
+	timestamps.set(interaction.user.id, now);
+	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(token);
+
+
+//ceci est un bot discord qui permet de recuperer les derniers articles d'un flux rss et de les envoyer sur un channel discord
+//il est possible de rajouter des flux rss avec une commande, aussi possible de supprimer des flux rss avec une commande
+//il est possible de recuperer les derniers articles d'un flux rss avec une commande
+//il est possible de recuperer les derniers articles de tous les flux rss avec une commande
+//il faudra que le bot recupere l'id du channel au moment ou on rajoute un flux pour pouvoir envoyer les articles sur le bon channel
+//une fois un flux rss mis le bot doit checker le flux toutes les 5 minutes et envoyer les nouveaux articles sur le channel
+
