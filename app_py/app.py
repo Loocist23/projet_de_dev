aa41d888521b68fe6df8d8623890a184df2299da
Anthony Zegnal <anthonyzegnal05@gmail.com>
2023-07-22 15:51:12 +0200

    test des differente chose fesable

---
+++ app_py/app.py
@@ -0,0 +1,31 @@
+from tkinter import *
+from tkinter import ttk
+
+def change_text():
+    #recupere les valeurs du champs Entry
+    value = Entry.get()
+    #on affiche la valeur dans le label
+    label.config(text=value)
+
+
+root = Tk()
+root.title("App Python")
+
+# Label
+label = ttk.Label(root, text="Hello World")
+
+# Button
+
+Button = ttk.Button(root, text="Click Me", command=change_text)
+
+# Entry
+
+Entry = ttk.Entry(root, width=30)
+
+#now we create the app
+
+label.pack()
+Button.pack()
+Entry.pack()
+
+root.mainloop()
