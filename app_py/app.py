135658739496ee37945923a031016af1c4feb44d
Anthony Zegnal <anthonyzegnal05@gmail.com>
2023-07-22 16:15:43 +0200

    rajout check si int

--- app_py/app.py
+++ app_py/app.py
@@ -1,31 +1,55 @@
 from tkinter import *
 from tkinter import ttk
+import random
+import string
 
-def change_text():
-    #recupere les valeurs du champs Entry
-    value = Entry.get()
-    #on affiche la valeur dans le label
-    label.config(text=value)
+def generate_password(size):
+    password = []
+    for i in range(size):
+        alpha = random.choice(string.ascii_letters)
+        symbol = random.choice(string.punctuation)
+        numbers = random.choice(string.digits)
+        password.append(random.choice([alpha, symbol, numbers]))
+    y = "".join(str(x) for x in password)
+    passwordlabel.config(text="Password: " + y)
+    
+def check():
+    try:
+        value = int(NumEntry.get())
+    except ValueError:
+        passwordlabel.config(text="Please enter a number")
+        return
+    if value < 8:
+        passwordlabel.config(text="Password too short")
+    elif value > 128:
+        passwordlabel.config(text="Password too long")
+    else:
+        generate_password(int(NumEntry.get()))
 
+# Create the window
 
 root = Tk()
 root.title("App Python")
 
 # Label
 label = ttk.Label(root, text="Generate a password")
+sizelabel = ttk.Label(root, text="Size, min: 8, max: 128")
+passwordlabel = ttk.Label(root, text="Password: ")
 
 # Button
 
-Button = ttk.Button(root, text="Click Me", command=change_text)
+GenButton = ttk.Button(root, text="Click Me", command=check)
 
 # Entry
 
-Entry = ttk.Entry(root, width=30)
+NumEntry = ttk.Entry(root)
 
 #now we create the app
 
 label.pack()
-Button.pack()
-Entry.pack()
+sizelabel.pack()
+GenButton.pack()
+NumEntry.pack()
+passwordlabel.pack()
 
 root.mainloop()
