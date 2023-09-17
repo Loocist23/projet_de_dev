80f41400938a8c02d5daf1743af06ea136818bbd
Loocist <anthonyzegnal05@gmail.com>
2023-09-17 22:56:00 +0200

    creation du projet mine_crawler

---
+++ mini_crawler.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+MAX_DEPTH = 3
+
+def check_url(url, base_url):
+    if url.startswith("http://") or url.startswith("https://"):
+        return url
+    elif url == "" or url.startswith("javascript:") or url.startswith("#"):
+        return None
+    else:
+        return base_url + url
+
+def scrap(url, depth=0, visited=None):
+    if visited is None:
+        visited = set()
+
+    if depth > MAX_DEPTH:
+        print("Profondeur maximale atteinte")
+        return
+
+    url = check_url(url, "http://")
+    if not url:
+        print("L'url est invalide ou vide")
+        return
+
+    if url in visited:
+        print(f"URL déjà visité: {url}")
+        return
+
+    visited.add(url)
+
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        print(f"Impossible d'accéder à l'URL: {url}")
+        return
+
+    soup = BeautifulSoup(r.text, "html.parser")
+    links = soup.find_all("a")
+
+    links_dict = {}
+
+    for link in links:
+        text = link.text
+        href = link.get("href")
+
+        full_url = check_url(href, url)
+        if full_url and full_url not in visited:
+            links_dict[text] = full_url
+            scrap(full_url, depth+1, visited)
+
+    with open(f"links_depth_{depth}.json", "w") as f:
+        json.dump(links_dict, f, indent=4)
+        print(f"Fichier links_depth_{depth}.json créé")
+
+url = input("Entrez l'URL à crawler : ")
+scrap(url)
--- test.py
+++ test.py
@@ -1,7 +1,28 @@
-import os
-import shutil
+import json, os, sys
+import yaml
 
-oui ="C:/Users/moi/Desktop/projet_de_dev/oui.txt"
-dossier = "C:/Users/moi/Desktop/projet_de_dev/oui/"
+#check si on nous a donner quelque chose dans la ligne de commande
 
-os.replace(oui, dossier)
+if len(sys.argv) > 1:
+    fichier = sys.argv[1]
+else:
+    fichier = input("Entrez le nom du fichier à ouvrir : ")
+
+#verifie si le fichier est deja existant #et si il est bien formaté
+
+
+if os.path.exists(fichier):
+    try:
+        with open(fichier, "r") as f:
+            data = json.load(f)
+            print("Le fichier est au format JSON")
+            #transformer le JSON en yaml
+            with open("test.yaml", "w") as f:
+                yaml.dump(data, f, default_flow_style=False, encoding='utf-8', allow_unicode=True)
+                print("Le fichier a bien été converti en YAML")
+
+    except:
+        print("Le fichier n'est pas au format JSON")
+else:
+    print("Le fichier n'existe pas")
+        
