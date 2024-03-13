b99b7e0586f56ac32a92cfce8046b2035f163bb4
Loocist <anthonyzegnal05@gmail.com>
2024-03-13 16:01:41 +0100

    test

--- amuse.py
+++ amuse.py
@@ -12,7 +12,7 @@ def generate_random_id():
 # Fonction pour afficher le texte en rouge, sauf si le solde est positif
 def print_red_unless_positive(text, solde):
     if solde > 0:
-        print(text)
+        print(Fore.GREEN + text)
     else:
         print(Fore.RED + text)
 
@@ -24,25 +24,26 @@ def generate_card_pin():
 
 def generate_soldes():
     # Génère un solde égal à zéro avec une probabilité de 99.99999%
-    if random.random() < 0.9999999:
+    if random.random() < 0.9999:
         return "0"
     # Si la probabilité n'est pas satisfaite, génère un solde positif aléatoire
-    return str(random.randint(1, 1e9))
+    return str(random.randint(1, 1e4))
 
 def prod():
+    random_soldes = generate_soldes()
     random_id = generate_random_id()
-    print_red_unless_positive("Identifiant : " + random_id, 0.0)
+    print_red_unless_positive("Identifiant : " + random_id, float(random_soldes))
 
     # Génère un numéro de carte et l'affiche en rouge
     random_card_number = generate_card_number()
-    print_red_unless_positive("Numéro de carte : " + random_card_number, 0.0)
+    print_red_unless_positive("Numéro de carte : " + random_card_number, float(random_soldes))
 
     # Génère un code PIN et l'affiche en rouge
     random_card_pin = generate_card_pin()
-    print_red_unless_positive("Code PIN : " + random_card_pin, 0.0)
+    print_red_unless_positive("Code PIN : " + random_card_pin, float(random_soldes))
 
     # Génère un solde et l'affiche en rouge, sauf s'il est égal à zéro
-    random_soldes = generate_soldes()
+    
     print_red_unless_positive("Solde : " + random_soldes, float(random_soldes))
 
 
